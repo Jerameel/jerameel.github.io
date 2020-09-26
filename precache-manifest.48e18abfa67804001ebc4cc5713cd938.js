42c387155ddba73cec9e3769ee5ab3379c32cd54
Jerameel <jerameeldelosreyes@gmail.com>
2020-09-26 10:23:59 +0000

    Updates

---
+++ precache-manifest.48e18abfa67804001ebc4cc5713cd938.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0dbe979abdc0676ae1884112a09350a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96a0c90a00fbab2b6bd9",
+    "url": "/static/css/main.cb7f129d.chunk.css"
+  },
+  {
+    "revision": "a6b8b264b4d8b4f2ff7f",
+    "url": "/static/js/2.e58a6761.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.e58a6761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96a0c90a00fbab2b6bd9",
+    "url": "/static/js/main.b924aef9.chunk.js"
+  },
+  {
+    "revision": "91238e9809f630d4dfc9",
+    "url": "/static/js/runtime-main.96d46733.js"
+  },
+  {
+    "revision": "fd5e5f32c1f438ecf5dd71d6ae01b049",
+    "url": "/static/media/Heebo-Bold.fd5e5f32.ttf"
+  },
+  {
+    "revision": "29bad8e0b0e6cf92f1f7d22f7fcfba79",
+    "url": "/static/media/Heebo-Light.29bad8e0.ttf"
+  },
+  {
+    "revision": "b4e0045784568bc212a3bb6de08891ff",
+    "url": "/static/media/Heebo-Medium.b4e00457.ttf"
+  },
+  {
+    "revision": "3f5fef64b9bb3ef6ded20c917f3082f7",
+    "url": "/static/media/Heebo-Regular.3f5fef64.ttf"
+  },
+  {
+    "revision": "e30823920fe7f8d820b5a80e54c30c84",
+    "url": "/static/media/Heebo-SemiBold.e3082392.ttf"
+  }
+]);
